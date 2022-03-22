537ff07d0f930c0ed02673d901248f2731ded901
Abhishek Ahlawat <abhishekahlawat64@gmail.com>
2022-03-22 16:43:56 +0530

    22-03-22 1663. Smallest String With A Given Numeric Value

--- 2212. Maximum Points in an Archery Competition/CPP/sol.cpp
+++ 2212. Maximum Points in an Archery Competition/CPP/sol.cpp
@@ -1,37 +1,48 @@
 public:
-    vector<int> maximumBobPoints(int numArrows, vector<int>& aliceArrows) {
-        
-        long long s= 0;
-        
-        long long mx=INT_MIN;
-        vector<int> ans(12); 
-        
-        rep(i, (1<<12)+1){
-            long long ns=0, flag=0, g=numArrows;
-            
-            rep(j,12){
-                
-                if((i & (1<<j))) {
-                    long long x= aliceArrows[j]+1;
-                          g-=x;
-                          ns+=j;
-                        if(g<0) { flag=1; break;}
+vector<int> maximumBobPoints(int numArrows, vector<int> &aliceArrows)
+{
 
+    long long s = 0;
+
+    long long mx = INT_MIN;
+    vector<int> ans(12);
+
+    fo(i, (1 << 12))
+    {
+        long long ns = 0, flag = 0, g = numArrows;
+
+        fo(j, 12)
+        {
+
+            if((i & (1 << j)))
+            {
+                long long x = aliceArrows[j] + 1;
+                g -= x;
+                ns += j;
+                if(g < 0)
+                {
+                    flag = 1;
+                    break;
+                }
+
+            }
+        }
+
+        if(flag == 0 && ns > mx )
+        {
+            fo(k, 12) ans[k] = 0;
+            fo(j, 12)
+            {
+                if((i & (1 << j)))
+                {
+                    ans[j] =  aliceArrows[j] + 1;
                 }
             }
-            
-            if(flag==0 && ns>mx ){
-                rep(k,12) ans[k]=0;
-                rep(j,12){
-                     if((i & (1<<j))){
-                         ans[j]=  aliceArrows[j]+1;
-                     }
-                }
-                if(g>0) ans[0]+=g;
-                mx=ns;
-            }
-         }
-        
-        return ans;
-        
+            if(g > 0) ans[0] += g;
+            mx = ns;
+        }
     }
+
+    return ans;
+
+}
